854adfb3c9ff3085c2ac05a3a66ff103f5866906
dusha <chayou.dusha@gmail.com>
2022-03-01 12:15:56 +0700

    test1

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL", "https://list-orders.herokuapp.com/"),
+  url: env("MY_HEROKU_URL"),
   proxy: true,
   app: {
     keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
